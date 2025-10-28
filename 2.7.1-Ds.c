17bbc273d48e376330d44a4d5c7afd66e06b67e0
IDududa <1966401308@qq.com>
2025-10-28 18:49:58 +0800

    2025.10.28
    顺序表线性表的笔记重新整理和补充，以及线性表操作bug的修改。

--- 2.7.1-Ds.c
+++ 2.7.1-Ds.c
@@ -31,7 +31,7 @@ Status InitList(SqList *L){
 
 Status ListInsert(SqList *L,int i,int e){
     //检查i的位置是否合法
-    if(i >= 1 && i <= L->length + 1)
+    if(i < 1 || i > L->length + 1)
     {
         return ERROR;
     }
@@ -44,7 +44,7 @@ Status ListInsert(SqList *L,int i,int e){
 
 
     //将插入位置后的元素依次向后移动
-    for(int j = L->length - 1;j >= 1;j--){
+    for(int j = L->length - 1;j >= i - 1 ;j--){
         L->data[j + 1] = L->data[j];
     }
 
@@ -56,36 +56,86 @@ Status ListInsert(SqList *L,int i,int e){
     
 }
 
+int GetElem(SqList *L,int i,int *e){
+    //检查i的位置是否合法
+    if(i<1 || i>L->length){
+        return ERROR;
+    }
+    *e = L->data[i - 1];
+    return OK;
+}
+
+Status LocateElem(SqList *L,int e){
+    for(int i = 1;i <= L->length;i++){
+        if(L->data[i - 1] == e){
+            return i;
+        }
+    }
+    return 0;
+}
+
+void MergList(SqList *A,SqList *B){
+    int al = A->length;
+    int bl = B->length;
+    for(int i = 1;i < bl ; i++){
+        int e = 0;
+        GetElem(B,i,&e);
+        if(!LocateElem(A,e))
+        {
+            ListInsert(A,al + 1,e);
+        }
+    }
+}
+
+void printList(SqList *L){
+    for(int i = 1;i <= L->length;i++){
+        printf("%d ",L->data[i - 1]);
+    }
+}
+
 int main(){
-    int A[10] = {7, 5, 3, 11};
-    int B[10] = {2,6,3};
+    int A[4] = {7, 5, 3, 11};
+    int B[3] = {2,6,3};
 
     int A_length = sizeof(A)/sizeof(A[0]);
+    printf("A的长度为：%d\n",A_length);
     int B_length = sizeof(B)/sizeof(B[0]);
+    printf("B的长度为：%d\n",B_length);
 
-    SqList *LA;
-    SqList *LB;
+    SqList *LA = (SqList *)malloc(sizeof(SqList));
+    SqList *LB = (SqList *)malloc(sizeof(SqList));
 
-    if(InitList(LA) != NULL)
+    if(InitList(LA) == OK)
     {
         printf("LA初始化完成\n");
     };
-    if (InitList(LB) != NULL)
+    if (InitList(LB) == OK)
     {
         printf("LB初始化完成\n");
     }
 
     for(int j = 1;j <= A_length;j++){
         ListInsert(LA,j,A[j-1]);
-        printf("插入成功\n");
     }
     
-    for(int j = 1;j <= A_length;j++){
+    for(int j = 1;j <= B_length;j++){
         ListInsert(LB,j,B[j-1]);
-        printf("插入成功\n");
     }
+    printf("A的元素为：");
+    printList(LA);
+    printf("\n");
+    printf("B的元素为：");
+    printList(LB);
+    printf("\n");
+    printf("合并后的元素为：");
+    MergList(LA,LB);
+    printList(LA);
+    printf("\n");
     
-
+    free(LA->data);
+    free(LB->data);
+    free(LA);
+    free(LB);
 
 
     return 0;
